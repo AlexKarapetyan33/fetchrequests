99b2cf5bf3fa5ed48c1c3dc10c3b9939d5fbb6b8
AlexKarapetyan33 <alexkarapetyan009@gmail.com>
2026-03-30 14:35:54 +0400

    render 20 props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 
 
 export function App() {
 
+  const [title, setTitle] = useState('')
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=20')
+      .then((res) => res.json())
+      .then((res) => setTodos(res))
+  }, [])
+
+  const changeTitle = (e) => {
+    setTitle(e.target.value)
+  }
+
 
   return (
     <>
-      
+      <input value={title} onChange={changeTitle}/>
+      <button>Add</button>
+
+      <ul>
+        {
+          todos.map((todo) => {
+            return(
+              <li key={todo.id}>
+                <b>{todo.id}</b>
+                  <span>{todo.title}</span>
+              </li>
+            )
+          })
+        }
+      </ul>
     </>
   )
 }
