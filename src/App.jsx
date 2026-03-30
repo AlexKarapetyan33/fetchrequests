18e831765e1eae630e6de5c249ed1a11bdb2bc6e
AlexKarapetyan33 <alexkarapetyan009@gmail.com>
2026-03-30 16:02:05 +0400

    removetodo

--- src/App.jsx
+++ src/App.jsx
@@ -16,24 +16,45 @@ export function App() {
     setTitle(e.target.value)
   }
 
+  const addTodo = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        completed: false
+      })
+    }).then((res) => res.json())
+      .then((res) => setTodos([res, ...todos]))
+  }
+
+  const removeTodo = (id) => {
+      fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method : 'DELETE'
+      })
+  }
 
   return (
     <>
-      <input value={title} onChange={changeTitle}/>
-      <button>Add</button>
+      <input value={title} onChange={changeTitle} />
+      <button onClick={addTodo}>Add</button>
 
       <ul>
         {
           todos.map((todo) => {
-            return(
+            return (
               <li key={todo.id}>
                 <b>{todo.id}</b>
-                  <span>{todo.title}</span>
+                <span>{todo.title}</span>
+                <button onClick={() => removeTodo(todo.id)}>X</button>
               </li>
             )
           })
         }
       </ul>
+
     </>
   )
 }
